fca86c44f3c37cc383f2cbd35471c796fc2c81eb
ma.zhiwu <ma.zhiwu@99cloud.net>
2024-01-14 19:04:28 +0800

    Initial Commit

---
+++ dylib_dobby_hook/dylib_dobby_hook.h
@@ -0,0 +1,12 @@
+//
+//  dylib_dobby_hook.h
+//  dylib_dobby_hook
+//
+//  Created by artemis on 2024/1/14.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dylib_dobby_hook : NSObject
+
+@end
---
+++ dylib_dobby_hook/dylib_dobby_hook.m
@@ -0,0 +1,12 @@
+//
+//  dylib_dobby_hook.m
+//  dylib_dobby_hook
+//
+//  Created by artemis on 2024/1/14.
+//
+
+#import "dylib_dobby_hook.h"
+
+@implementation dylib_dobby_hook
+
+@end
