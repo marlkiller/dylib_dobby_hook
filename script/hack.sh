d464ad82723a4a584081933c7d4c233dc707cb85
ma.zhiwu <ma.zhiwu@99cloud.net>
2024-01-27 16:46:12 +0800

    fixed: 文件名大小写修复

--- script/hack.sh
+++ script/hack.sh
@@ -8,7 +8,7 @@ prefix="lib"
 
 insert_dylib="${current_path}/../tools/insert_dylib"
 
-BUILT_PRODUCTS_DIR="${current_path}/../Release"
+BUILT_PRODUCTS_DIR="${current_path}/../release"
 
 app_bundle_path="/Applications/${app_name}.app/Contents/MacOS/"
 
