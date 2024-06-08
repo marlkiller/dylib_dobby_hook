315a691dcd662a5625462848641e487524deb1b4
artemis <marlkiller@voidm.com>
2024-06-08 08:59:19 +0800

    Support TablePlus Build 558
    closed: #16
    (cherry picked from commit bd9bde28bd5f206c09e1cd22d55748abb3f33eb5)

--- dylib_dobby_hook/apps/TablePlusHack.m
+++ dylib_dobby_hook/apps/TablePlusHack.m
@@ -26,6 +26,7 @@ static IMP urlWithStringSeletorIMP;
 static IMP NSURLSessionClassIMP;
 
 
+
 - (NSString *)getAppName {
     return @"com.tinyapp.TablePlus";
 }
@@ -35,150 +36,125 @@ static IMP NSURLSessionClassIMP;
 }
 
 
-const LicenseModel *_rbx;
+//__strong id _rbx;
+//
+//id hook_license(int arg0, int arg1, int arg2, int arg3){
+//    if (_rbx==nil){
+//        
+//        // 通过反射获取 Swift 类
+//        Class TBLicenseModelClass = NSClassFromString(@"_TtC9TablePlus14TBLicenseModel");
+//        if (!TBLicenseModelClass) {
+//           return nil;
+//        }
+//       
+//        id r12 = [TBLicenseModelClass alloc] ;
+//
+//        [MemoryUtils listAllPropertiesMethodsAndVariables:TBLicenseModelClass];
+//        // LicenseModel *r12 = [[NSClassFromString(@"LicenseModel") alloc] init];
+//        NSString *deviceId = [EncryptionUtils generateTablePlusDeviceId];
+////        [r12 setValue:deviceId forKey:@"deviceID"];
+//        [MemoryUtils setInstanceIvar:r12 ivarName:@"deviceID" value:deviceId];
+//        [MemoryUtils setInstanceIvar:r12 ivarName:@"sign" value:@"12345678901234567890123456789012345678901234567890"];
+//        [MemoryUtils setInstanceIvar:r12 ivarName:@"purchasedAt" value:@"2999-01-16"];
+//        [MemoryUtils setInstanceIvar:r12 ivarName:@"updatesAvailableUntil" value:@"2999-01-16"];
+//        [MemoryUtils setInstanceIvar:r12 ivarName:@"licenseKey" value:@"licenseKey"];
+//        [MemoryUtils setInstanceIvar:r12 ivarName:@"nextChargeAt" value:@"123456"];
+//        [MemoryUtils setInstanceIvar:r12 ivarName:@"email" value:[NSString stringWithCString:global_email_address encoding:NSUTF8StringEncoding]];
+//        
+//        // 获取属性名对应的 Ivar
+//       Ivar ivar = class_getInstanceVariable([TBLicenseModelClass class], "deviceID");
+//       // 如果 ivar 不为空，说明属性存在
+//       if (ivar != NULL) {
+//           // 获取属性的偏移量
+//           ptrdiff_t offset = ivar_getOffset(ivar);
+//           
+//           uintptr_t address = (uintptr_t)(__bridge void *)r12 + offset;
+//           // 计算属性在对象中的地址
+//           NSString * __autoreleasing *deviceIdPtr = (NSString * __autoreleasing *)(void *)address;
+//           *deviceIdPtr = deviceId;
+//       }
+//        
+//        Ivar sign = class_getInstanceVariable([TBLicenseModelClass class], "sign");
+//        if (sign != NULL) {
+//            ptrdiff_t offset = ivar_getOffset(sign);
+//            uintptr_t address = (uintptr_t)(__bridge void *)r12 + offset;
+//            NSString * __autoreleasing *deviceIdPtr = (NSString * __autoreleasing *)(void *)address;
+//            *deviceIdPtr = deviceId;
+//        }
+//        
+//       _rbx=r12;
+//        NSLog(@">>>>>> deviceId: %@",deviceId);
+////        _rbx=r12;
+////        return r12;
+//        // rax_12.b = rax_11 s>= 0x32
+////        NSDictionary *propertyDictionary = @{
+////            @"sign": @"12345678901234567890123456789012345678901234567890",
+////            @"email": [NSString stringWithCString:global_email_address encoding:NSUTF8StringEncoding],
+////            @"deviceID": deviceId,
+////            @"licenseKey": @"licenseKey",
+////            @"purchasedAt": @"2999-01-16",
+////            @"nextChargeAt": @(9999999999999), // Replace with the actual double value
+////            @"updatesAvailableUntil": @"2999-01-16" // Replace with the actual value
+////        };
+////        _rbx = [r12 initWithDictionary:propertyDictionary];;
+//    }
+//    return _rbx;
+//}
 
-id hook_license(int arg0, int arg1, int arg2, int arg3){
-    if (_rbx==nil){
-        LicenseModel *r12 = [[NSClassFromString(@"LicenseModel") alloc] init];
-        NSString *deviceId = [EncryptionUtils generateTablePlusDeviceId];
-        NSLog(@">>>>>> deviceId: %@",deviceId);
-        // rax_12.b = rax_11 s>= 0x32
-        NSDictionary *propertyDictionary = @{
-            @"sign": @"12345678901234567890123456789012345678901234567890",
-            @"email": [NSString stringWithCString:global_email_address encoding:NSUTF8StringEncoding],
-            @"deviceID": deviceId,
-            @"purchasedAt": @"2999-01-16",
-            @"nextChargeAt": @(9999999999999), // Replace with the actual double value
-            @"updatesAvailableUntil": @"2999-01-16" // Replace with the actual value
-        };
-        _rbx = [r12 initWithDictionary:propertyDictionary];;
-    }
-    return _rbx;
-}
-
-
-bool hook_device_id(uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4){
-    //
-    // Interceptor.attach(Module.findExportByName(null, 'CC_MD5')
-    // ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'
-    // system_profiler SPHardwareDataType | grep Serial
-    // deviceID = md5(mac+Serial)
-    // 3c:06:30:30:7d:35C02G64QMQ05D
-    // 88548E5A38EEEE04E89C5621BA04BC7E
-    
-    if (_rbx!=nil && [_rbx.deviceID isEqual:@""]){
-        
-        // arm: r1 = *(int128_t *)(arg2 + 0x28);
-        // x86: mov  rsi, qword [rdx+0x28] ; real device id ; rsi = *(arg2 + 0x28);
-        
-        // 地址要倒叙处理,垃圾写法
-        // uint64_t _rsi = (arg2 + 0x28);
-        // NSString * addressString = [MemoryUtils readMachineCodeStringAtAddress:(_rsi) length:(8)];
-        // NSArray<NSString *> *byteStrings = [addressString componentsSeparatedByString:@" "];
-        // NSMutableArray<NSString *> *reversedByteStrings = [NSMutableArray arrayWithCapacity:byteStrings.count];
-        // // 将字节字符串倒序
-        // for (NSInteger i = byteStrings.count - 1; i >= 0; i--) {
-        //     [reversedByteStrings addObject:byteStrings[i]];
-        // }
-        // // 连接倒序后的字节字符串
-        // NSString *reversedAddressString = [reversedByteStrings componentsJoinedByString:@""];
-        // // 将倒序后的地址字符串转换为实际地址值
-        // unsigned long long address = strtoull([reversedAddressString UTF8String], NULL, 16);
-        // void *addressPtr = (void *)address;
-        
-        
-        // 虽然看不明白, 但是这个写法短小精干
-        // memory read ptr = 00 0f 3c 00 00 60 00 00,
-        // memory read *ptr *ptr+100 = deviceId
-        // addressPtr = 60 00 00 3c 0f 00
-        uintptr_t *ptr = (uintptr_t *)(arg2 + 0x28);
-        // NSLog(@">>>>>> ptr: %#lx", ptr);
-        // 将 ptr 指向的内存地址的值（即指针所指向的地址）赋值给 addressPtr
-        void * addressPtr = (void *) *ptr;
-        // [MemoryUtils inspectObjectWithAddress:addressPtr]; // 打印对象
-        // NSString * deviceId = [MemoryUtils readStringAtAddress:(addressPtr+0x20)];
-        // NSLog(@">>>>>> deviceId: %@", deviceId);
-        // _rbx.deviceID =deviceId;
-        NSString *deviceId = [NSString stringWithCString:addressPtr+0x20 encoding:NSUTF8StringEncoding];
-        _rbx.deviceID =deviceId;
-        NSLog(@">>>>>> deviceId: %@", deviceId);
-        
-    }
-    return hook_device_id_ori(arg0,arg1,arg2,arg3,arg4);
-}
-
-int (*hook_license_ori)(void);
-
-int (*hook_device_id_ori)(uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4);
+//
+//bool hook_device_id(uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4){
+//    //
+//    // Interceptor.attach(Module.findExportByName(null, 'CC_MD5')
+//    // ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'
+//    // system_profiler SPHardwareDataType | grep Serial
+//    // deviceID = md5(mac+Serial)
+//    // 3c:06:30:30:7d:35C02G64QMQ05D
+//    // 88548E5A38EEEE04E89C5621BA04BC7E
+//    
+//    if (_rbx!=nil && [_rbx.deviceID isEqual:@""]){
+//        
+//        // arm: r1 = *(int128_t *)(arg2 + 0x28);
+//        // x86: mov  rsi, qword [rdx+0x28] ; real device id ; rsi = *(arg2 + 0x28);
+//        
+//        // 地址要倒叙处理,垃圾写法
+//        // uint64_t _rsi = (arg2 + 0x28);
+//        // NSString * addressString = [MemoryUtils readMachineCodeStringAtAddress:(_rsi) length:(8)];
+//        // NSArray<NSString *> *byteStrings = [addressString componentsSeparatedByString:@" "];
+//        // NSMutableArray<NSString *> *reversedByteStrings = [NSMutableArray arrayWithCapacity:byteStrings.count];
+//        // // 将字节字符串倒序
+//        // for (NSInteger i = byteStrings.count - 1; i >= 0; i--) {
+//        //     [reversedByteStrings addObject:byteStrings[i]];
+//        // }
+//        // // 连接倒序后的字节字符串
+//        // NSString *reversedAddressString = [reversedByteStrings componentsJoinedByString:@""];
+//        // // 将倒序后的地址字符串转换为实际地址值
+//        // unsigned long long address = strtoull([reversedAddressString UTF8String], NULL, 16);
+//        // void *addressPtr = (void *)address;
+//        
+//        
+//        // 虽然看不明白, 但是这个写法短小精干
+//        // memory read ptr = 00 0f 3c 00 00 60 00 00,
+//        // memory read *ptr *ptr+100 = deviceId
+//        // addressPtr = 60 00 00 3c 0f 00
+//        uintptr_t *ptr = (uintptr_t *)(arg2 + 0x28);
+//        // NSLog(@">>>>>> ptr: %#lx", ptr);
+//        // 将 ptr 指向的内存地址的值（即指针所指向的地址）赋值给 addressPtr
+//        void * addressPtr = (void *) *ptr;
+//        // [MemoryUtils inspectObjectWithAddress:addressPtr]; // 打印对象
+//        // NSString * deviceId = [MemoryUtils readStringAtAddress:(addressPtr+0x20)];
+//        // NSLog(@">>>>>> deviceId: %@", deviceId);
+//        // _rbx.deviceID =deviceId;
+//        NSString *deviceId = [NSString stringWithCString:addressPtr+0x20 encoding:NSUTF8StringEncoding];
+//        _rbx.deviceID =deviceId;
+//        NSLog(@">>>>>> deviceId: %@", deviceId);
+//        
+//    }
+//    return hook_device_id_ori(arg0,arg1,arg2,arg3,arg4);
+//}
 
 
 - (BOOL)hack {
     
-    NSString *searchFilePath = [[Constant getCurrentAppPath] stringByAppendingString:@"/Contents/MacOS/TablePlus"];
-    
-    
-#if defined(__arm64__) || defined(__aarch64__)
-    
-    
-    
-
-//    000000010013ca50         stp        x28, x27, [sp, #-0x60]!                     ; CODE XREF=sub_10013d2c4+1360, sub_1002cc010+8
-//    000000010013ca54         stp        x26, x25, [sp, #0x10]
-//    000000010013ca58         stp        x24, x23, [sp, #0x20]
-//    000000010013ca5c         stp        x22, x21, [sp, #0x30]
-//    000000010013ca60         stp        x20, x19, [sp, #0x40]
-//    000000010013ca64         stp        fp, lr, [sp, #0x50]
-//    000000010013ca68         add        fp, sp, #0x50
-//    000000010013ca6c         sub        sp, sp, #0x70
-//    000000010013ca70         adrp       x0, #0x10087a000                            ; 0x10087aeb0@PAGE
-//    000000010013ca74         add        x0, x0, #0xeb0                              ; 0x10087aeb0@PAGEOFF, argument #1 for method sub_10001310c, qword_10087aeb0
-//    000000010013ca78         bl         sub_10001310c                               ; sub_10001310c
-//    000000010013ca7c         ldur       x8, [x0, #-0x8]                             ; qword_10087aea8
-//    000000010013ca80         ldr        x8, [x8, #0x40]
-//    000000010013ca84         mov        x9, sp
-//    000000010013ca88         add        x8, x8, #0xf
-//    000000010013ca8c         and        x8, x8, #0xfffffffffffffff0
-//    000000010013ca90         sub        x24, x9, x8
-    
-//    FC 6F BA A9 FA 67 01 A9 F8 5F 02 A9 F6 57 03 A9 F4 4F 04 A9 FD 7B 05 A9 FD 43 01 91 FF C3 01 D1 E0 39 00 D0 00 C0 3A 91 A5 59 FB 97 08 80 5F F8 08 21 40 F9 E9 03 00 91 08 3D 00 91 08 ED 7C 92 38 01 08 CB
-    //  objc_cls_ref_LicenseModel
-    NSArray *globalOffsets =[MemoryUtils searchMachineCodeOffsets:(NSString *)searchFilePath
-                                                      machineCode:(NSString *) @"FC 6F BA A9 FA 67 01 A9 F8 5F 02 A9 F6 57 03 A9 F4 4F 04 A9 FD 7B 05 A9 FD 43 01 91 FF .. .. D1 .. .. 00 .. .. .. .. .. .. .. .. .. .. .. 5F F8 .. .. 40 F9 E9 03 00 91 08 3D 00 91 08 ED 7C 92 38 01 08 CB"
-                                                            count:(int)1];
-    uintptr_t globalOffset = [globalOffsets[0] unsignedIntegerValue];
-    uintptr_t fileOffset =[MemoryUtils getCurrentArchFileOffset: searchFilePath];
-    
-    hook_license(1,2,3,4);
-    
-    intptr_t _hook_license = [MemoryUtils getPtrFromGlobalOffset:0 targetFunctionOffset:(uintptr_t)globalOffset reduceOffset:(uintptr_t)fileOffset];
-    DobbyHook((void *)_hook_license, (void *)hook_license, (void *)&hook_license_ori);
-   
-#elif defined(__x86_64__)
-    
-    //    000000010014af90         push       rbp                                         ; CODE XREF=sub_10014b5d0+1351, sub_1002fa1f0+4
-    //    000000010014af91         mov        rbp, rsp
-    //    000000010014af94         push       r15
-    //    000000010014af96         push       r14
-    //    000000010014af98         push       r13
-    //    000000010014af9a         push       r12
-    //    000000010014af9c         push       rbx
-    //    000000010014af9d         sub        rsp, 0x98
-    //    000000010014afa4         lea        rdi, qword [qword_1008ad698+8]              ; argument #1 for method sub_100015360, 0x1008ad6a0
-    //    000000010014afab         call       sub_100015360
-    //    55 48 89 E5 41 57 41 56 41 55 41 54 53 48 81 EC 98 00 00 00 48 8D 3D F5 26 76 00 E8
-    
-    NSString *searchMachineCode = @"55 48 89 E5 41 57 41 56 41 55 41 54 53 48 81 EC .. .. .. .. 48 8D 3D .. .. .. .. E8 .. .. .. .. 48 8B 40 .. 48 8B 40 .. 48 89 E3 48 83 C0 .. 48 83 E0 .. 48 29 C3 48 89 DC 31 FF E8 .. .. .. ..";
-    int count = 1;
-    NSArray *globalOffsets =[MemoryUtils searchMachineCodeOffsets:(NSString *)searchFilePath machineCode:(NSString *)searchMachineCode count:(int)count];
-    uintptr_t globalOffset = [globalOffsets[0] unsignedIntegerValue];
-    uintptr_t fileOffset =[MemoryUtils getCurrentArchFileOffset: searchFilePath];
-    
-    intptr_t _hook_license = [MemoryUtils getPtrFromGlobalOffset:0 targetFunctionOffset:(uintptr_t)globalOffset reduceOffset:(uintptr_t)fileOffset];
-    DobbyHook((void *)_hook_license, (void *)hook_license, (void *)&hook_license_ori);
-   
-#endif
-    
-    hook_license(1,2,3,4);
     
     Class NSURLControllerClass = NSClassFromString(@"NSURL");
     SEL urlWithStringSeletor = NSSelectorFromString(@"URLWithString:");
@@ -191,10 +167,65 @@ int (*hook_device_id_ori)(uint64_t arg0, uint64_t arg1, uint64_t arg2, uint64_t 
                 swizzledSelector:NSSelectorFromString(@"hk_URLWithString:")
     ];
     
+    
+//    echo "fuck" > ~/Library/Application\ Support/com.tinyapp.TablePlus/.licensemac
+//    获取 NSFileManager 的单例
+//    0x100149e44 <+340>:  movq   0x725fa5(%rip), %rsi      ; "fileExistsAtPath:"
+//    0x100149e4b <+347>:  movq   %rbx, %rdi
+//    0x100149e4e <+350>:  movq   %r12, %rdx
+//->  0x100149e51 <+353>:  callq  0x1006e641a               ; symbol stub for: objc_msgSend
+    
+    // 获取用户的 Application Support 目录路径
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+    NSString *applicationSupportDirectory = [paths firstObject];
+    // 获取当前应用程序的 bundle identifier
+    NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+    // 构建完整的路径
+    NSString *appDirectory = [applicationSupportDirectory stringByAppendingPathComponent:bundleIdentifier];
+    NSString *licensePath = [appDirectory stringByAppendingPathComponent:@".licensemac"];
+    
+    NSLog(@">>>>>> License file path: %@", licensePath);
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    BOOL fileExists = [fileManager fileExistsAtPath:licensePath];
+    
+    if (!fileExists) {
+        NSString *licenseContent = @"?";
+        BOOL success = [licenseContent writeToFile:licensePath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+        NSLog(@">>>>>> License file: %hhd",success);
+    }
+    
+    
+    // r12 = [[RNDecryptor decryptData:"file bytes" withPassword:"x" error:&var_48] retain];
+    // +[RNDecryptor decryptData:withPassword:error:]:
+    [MemoryUtils hookClassMethod:
+         NSClassFromString(@"RNDecryptor")
+                originalSelector: NSSelectorFromString(@"decryptData:withPassword:error:")
+                   swizzledClass:[self class]
+                swizzledSelector:NSSelectorFromString(@"hk_decryptData:withPassword:error:")
+    ];
+    
+    // -[_TtC9TablePlus13LicenseWindow licenseChanged]:
+    
+    
     return YES;
 }
 
 
++ (id) hk_decryptData:arg1 withPassword:(NSString *)withPassword error:(int)error{
+    NSDictionary *propertyDictionary = @{
+        @"sign": @"12345678901234567890123456789012345678901234567890",
+        @"email": [NSString stringWithCString:global_email_address encoding:NSUTF8StringEncoding],
+        @"deviceID":[EncryptionUtils generateTablePlusDeviceId],
+        @"licenseKey": @"licenseKey",
+        @"purchasedAt": @"2025-06-16",
+        @"nextChargeAt": @"2025-06-16",
+        @"updatesAvailableUntil": @"2025-06-16"
+    };
+
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:propertyDictionary options:0 error:nil];
+    return jsonData;
+}
 
 + (id)hk_URLWithString:arg1{
     
