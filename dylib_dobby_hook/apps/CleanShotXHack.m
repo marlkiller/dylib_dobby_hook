4b02f575cad00a5f389325d02af5329126cd39a6
Anonymous <143768885+anonaustin@users.noreply.github.com>
2025-04-20 13:40:12 +0530

    Update CleanShotXHack.m
    Support for 4.7.6 version.

--- dylib_dobby_hook/apps/CleanShotXHack.m
+++ dylib_dobby_hook/apps/CleanShotXHack.m
@@ -42,9 +42,9 @@
     tiny_hook(showCleanShotAWC, ret0, NULL);
     
 #if defined(__arm64__) || defined(__aarch64__)
-    NSString *checkHex = @"A8 EC 78 D3 BF 00 43 F2 88 00 88 9A 07 BD 40 92 08 FC 50 D3 29 FC 50 D3 5F 00 04 EB";
+    NSString *checkHex = @"A8 EC 78 D3 89 BC 40 92 BF 00 43 F2 27 01 88 9A 08 FC 50 D3 29 FC 50 D3 5F 00 04 EB"; // 27 01 88 9A 08 FC 50 D3 29 FC 50 D3 5F 00 04 EB is not necessary
 #elif defined(__x86_64__)
-    NSString *checkHex = @"48 89 F0 49 89 FA 4C 89 CE 48 C1 EE 38 83 E6 0F 49 0F BA E1 3D 49 0F 43 F0 49 BB FF FF FF FF FF FF 00 00 49 21 F3 48 C1 E8 10 4C 39 C2";
+    NSString *checkHex = @"48 89 F0 49 89 FA 4D 89 CB 49 C1 EB"; // 4D 89 CB 49 C1 EB is not necessary
 #endif
     [MemoryUtils hookWithMachineCode:@"/Contents/MacOS/CleanShot X"
                              machineCode:checkHex
@@ -52,7 +52,7 @@
                                    count:1];
     
 #if defined(__arm64__) || defined(__aarch64__)
-    NSString *sigaMachCode = @"FF 03 02 D1 E9 23 03 6D F8 5F 04 A9 F6 57 05 A9 F4 4F 06 A9 FD 7B 07 A9 FD C3 01 91 .. .. .. .. .. .. 41 F9 .. .. 00 B4";
+    NSString *sigaMachCode = @"FF 03 02 D1 E9 23 03 6D F8 5F 04 A9 F6 57 05 A9 F4 4F 06 A9 FD 7B 07 A9 FD C3 01 91 .. .. .. .. .. .. .. F9 .. .. 00 B4";
     [MemoryUtils hookWithMachineCode:@"/Contents/MacOS/CleanShot X"
                              machineCode:sigaMachCode
                                fake_func:(void *)ret
