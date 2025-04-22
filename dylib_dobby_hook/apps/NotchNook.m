84dc2ed21c17179f827f020df3bf01b39aff90d4
NKR00711 <esensei@duck.com>
2025-04-23 00:04:32 +0530

    NotchNook
    Infinite Trial in NotchNook
    
    (cherry picked from commit 224641eef96b310843073c0cd6e9cd57e2d005b7)

---
+++ dylib_dobby_hook/apps/NotchNook.m
@@ -0,0 +1,66 @@
+//
+//  NotchNook.m
+//  dylib_dobby_hook
+//
+//  Created by NKR on 2025/4/23.
+//
+
+#import <Foundation/Foundation.h>
+#import "Constant.h"
+#import "tinyhook.h"
+#import "MemoryUtils.h"
+#import <objc/runtime.h>
+#include <sys/ptrace.h>
+#import "common_ret.h"
+#import "HackProtocolDefault.h"
+
+@interface NotchNook : HackProtocolDefault
+
+@end
+
+@implementation NotchNook
+
+
+- (NSString *)getAppName {
+    return @"lo.cafe.NotchNook";
+}
+
+- (NSString *)getSupportAppVersion {
+    return @"";
+}
+
+static IMP hookNSUserDefaultsobjectForKeyIMP;
+
++ (NSNumber *)updateTrialInitialTimeIfNeeded {
+    NSString *defaultName = @"trialInitialTime";
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    NSInteger currentTimestamp = (NSInteger)[[NSDate date] timeIntervalSince1970];
+    NSInteger storedTimestamp = [defaults integerForKey:defaultName];
+    NSInteger difference = currentTimestamp - storedTimestamp;
+    if (difference >= 172800) {
+        [defaults setInteger:currentTimestamp forKey:defaultName];
+        [defaults synchronize];
+        return @(currentTimestamp);
+    }
+    return @(storedTimestamp);
+}
+
+- (id)swizzled_objectForKey:(NSString *)defaultName {
+    if ([defaultName isEqualToString:@"trialInitialTime"]) {
+      return [NotchNook updateTrialInitialTimeIfNeeded];
+     }
+    return ((id(*)(id, SEL, NSString *))hookNSUserDefaultsobjectForKeyIMP)(self, _cmd, defaultName);
+}
+
+- (BOOL)hack {
+
+  hookNSUserDefaultsobjectForKeyIMP = [MemoryUtils hookInstanceMethod:
+                          objc_getClass("NSUserDefaults")
+                   originalSelector:NSSelectorFromString(@"objectForKey:")
+                      swizzledClass:[self class]
+                        swizzledSelector:@selector(swizzled_objectForKey:)
+    ];
+
+    return YES;
+}
+@end
